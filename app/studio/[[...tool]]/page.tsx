977c21fb1878e3b2111372fdfb7b16ad6bbb8c51
behnamp <84948299+behnamp@users.noreply.github.com>
2026-07-01 12:38:49 +0330

    fix: drop next-sanity, use sanity Studio directly (Next.js 16 compat)

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,15 +2,15 @@
 import dynamic from 'next/dynamic'
 import config from '../../../sanity.config'
 
-const NextStudio = dynamic(
-  () => import('next-sanity/studio').then(m => m.NextStudio),
+const Studio = dynamic(
+  () => import('sanity').then(m => m.Studio),
   { ssr: false }
 )
 
 export default function StudioPage() {
   return (
     <div style={{ height: '100dvh' }}>
-      <NextStudio config={config} />
+      <Studio config={config} />
     </div>
   )
 }
